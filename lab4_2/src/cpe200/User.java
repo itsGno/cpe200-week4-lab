8d2acee319c8a7f67d48be52a1d2d1f48600a938
ISNE#3 1033 <no17615@hotmail.com>
2016-09-13 23:11:20 +0700

    580611033(NIPITPON DOUNGYAI) - cpe200-week4-lab

--- lab4_2/src/cpe200/User.java
+++ lab4_2/src/cpe200/User.java
@@ -1,28 +1,62 @@
 package cpe200;
 
+import java.util.ArrayList;
+
 /**
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    protected String userName;
+    protected String password;
 
-    public User() {
+    public User()
+    {
+        /* your code here */
     }
 
-    public boolean setUserName(String name) {
-        return false;
+    public boolean setUserName(String name)
+    {
+        if (!name.matches("(^[a-zA-Z]+)([a-zA-Z0-9]*)"))          return false;
+        if(name.length()<8)return false;
+        userName=name;
+        return true;
+        /* your code here */
     }
 
     public boolean setPassword(String name) {
-        return false;
+        int a = 0;
+        if (name.length() < 12) {
+            return false;
+        }
+        if (name.length() >= 12) {
+            a++;
+        }
+        if (name.matches("(.*)([0-9]+)(.*)")) {
+            a++;
+        }
+        if (name.matches("(.*)([a-z]+)(.*)")) {
+            a++;
+        }
+        if (name.matches("(.*)([A-Z]+)(.*)")) {
+            a++;
+        }
+        if (a == 4) {
+            password = name;
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public String getUserName() {
-        return null;
+    public String getUserName()
+    {
+        /* your code here */
+        return userName;
     }
 
-    public String getPassword() {
-        return null;
+    public String getPassword()
+    {
+        /* your code here */
+        return password;
     }
 }
--- lab4_2/src/cpe200/Users.java
+++ lab4_2/src/cpe200/Users.java
@@ -6,44 +6,67 @@ import java.util.ArrayList;
  * Created by pruet on 12/9/2559.
  */
 public class Users {
+
     public ArrayList<User> userList;
+
+    public Users(){
+        userList =new ArrayList<User>();
+    }
 
     public void addUser(User user)
     {
+        userList.add(user);
     }
 
     public void addUser(String userName, String password)
     {
+        User temp;
+        temp = new User();
+        temp.setUserName(userName);
+        temp.setPassword(password);
+        userList.add(temp);
     }
 
     public void deleteUser(User user)
     {
-
+        userList.remove(user);
     }
 
     public boolean exists(User user)
     {
+        if(userList.contains(user))
+            return true;
         return false;
     }
 
     public boolean usernameExists(String username)
     {
+
         return false;
     }
 
     /* This method should return null when the user with username is not in the list */
     public User getUserByUsername(String userName)
     {
+
+        for(int i =0;i<userList.size();i++)
+            if(userList.get(i).userName==userName){
+                return userList.get(i);
+            }
+
         return null;
     }
 
     public int count()
     {
-        return 0;
+        return userList.size();
     }
 
     public User[] getUserArray()
     {
-        return null;
+        User[] u = new User[userList.size()];
+        for(int i=0;i<userList.size();i++)
+           u[i]= userList.get(i);
+        return  u;
     }
 }
